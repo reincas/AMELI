c0531036a1dd6be69b99b999d5b890643da79844
reincas <r.caspary@posteo.de>
2026-01-20 17:03:01 +0100

    Added lanthanide_matrices()

--- ameli/__init__.py
+++ ameli/__init__.py
@@ -87,6 +87,32 @@ def desc_format(description, kwargs):
     return desc
 
 
+def lanthanide_matrices():
+    """ Return a list of names of all available matrices for lanthanide ions and the number of electron each is
+    acting on. """
+
+    names = []
+    names.extend([(f"U/{k},{q}", 1) for k in range(7) for q in range(-k, k + 1)])
+    names.extend([(f"T/{k},{q}", 1) for k in range(2) for q in range(-k, k + 1)])
+    names.extend([(f"UU/{k}", 2) for k in (0, 1, 2, 3, 4, 5, 6)])
+    names.extend([(f"TT/{k}", 2) for k in (0, 1)])
+    names.extend([(f"UT/{k}", 2) for k in (0, 1)])
+    names.extend([(f"L/{q}", 1) for q in range(-1, 2)])
+    names.extend([(f"S/{q}", 1) for q in range(-1, 2)])
+    names.extend([(f"J/{q}", 1) for q in range(-1, 2)])
+    names.append(("L2", 1))
+    names.append(("S2", 1))
+    names.append(("J2", 1))
+    names.append(("LS", 1))
+    names.extend([(f"H1/{k}", 2) for k in (2, 4, 6)])
+    names.append(("H2", 1))
+    names.extend([(f"H3/{i}", 2) for i in (0, 1, 2)])
+    names.extend([(f"H4/{c}", 3) for c in (2, 3, 4, 6, 7, 8)])
+    names.extend([(f"H5/{k}", 2) for k in (0, 2, 4)])
+    names.extend([(f"H6/{k}", 2) for k in (2, 4, 6)])
+    return names
+
+
 ##########################################################################
 # Import main classes
 ##########################################################################
