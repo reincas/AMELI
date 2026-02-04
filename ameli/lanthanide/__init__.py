0e5eef54a17efc54db48a25f6947712bdaa5d1ff
reincas <r.caspary@posteo.de>
2026-02-04 08:51:13 +0100

    Added lanthanide ion mapping

---
+++ ameli/lanthanide/__init__.py
@@ -0,0 +1,21 @@
+##########################################################################
+# Copyright (c) 2026 Reinhard Caspary                                    #
+# <reinhard.caspary@phoenixd.uni-hannover.de>                            #
+# This program is free software under the terms of the MIT license.      #
+##########################################################################
+
+LANTHANIDE_IONS = {
+    1: "Ce",
+    2: "Pr",
+    3: "Nd",
+    4: "Pm",
+    5: "Sm",
+    6: "Eu",
+    7: "Gd",
+    8: "Tb",
+    9: "Dy",
+    10: "Ho",
+    11: "Er",
+    12: "Tm",
+    13: "Yb",
+}
