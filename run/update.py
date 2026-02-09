cf42adf64d384c2927df4168aa01569834daedfb
reincas <r.caspary@posteo.de>
2026-02-09 22:07:13 +0100

    Load every data container to trigger an update.

--- run/update.py
+++ run/update.py
@@ -3,6 +3,12 @@
 # <reinhard.caspary@phoenixd.uni-hannover.de>                            #
 # This program is free software under the terms of the MIT license.      #
 ##########################################################################
+#
+# This script loads every data container in the vault to trigger a
+# potential container update.
+#
+##########################################################################
+
 from ameli.lanthanide.content import get_configs, update_containers
 
 for num_electrons in get_configs():
