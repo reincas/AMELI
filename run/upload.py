07b272b1e1688987ad83f45a1be780edd6efd1c1
reincas <r.caspary@posteo.de>
2026-02-13 07:43:37 +0100

    Upload of version-based Zenodo dataset records

--- run/upload.py
+++ run/upload.py
@@ -4,41 +4,29 @@
 # This program is free software under the terms of the MIT license.      #
 ##########################################################################
 
-import pprint
-import requests
-
 from ameli.lanthanide.content import get_configs
-from ameli.lanthanide.zenodo import GitHubConfig, zenodo_lanthanide
+from ameli.lanthanide.zenodo import zenodo_lanthanide
 
 SANDBOX = True
 RECORDS = {
-    #1: {"concept_id": None, "version": "1.0.0"},
-    1: {"concept_id": 440211, "version": "2.0.0"},
+    1: {"concept_id": None, "version": "1.0.0"},
+    2: {"concept_id": None, "version": "1.0.0"},
+    3: {"concept_id": None, "version": "1.0.0"},
+    4: {"concept_id": None, "version": "1.0.0"},
+    5: {"concept_id": None, "version": "1.0.0"},
+    6: {"concept_id": None, "version": "1.0.0"},
+    7: {"concept_id": None, "version": "1.0.0"},
+    8: {"concept_id": None, "version": "1.0.0"},
+    9: {"concept_id": None, "version": "1.0.0"},
+    10: {"concept_id": None, "version": "1.0.0"},
+    11: {"concept_id": None, "version": "1.0.0"},
+    12: {"concept_id": None, "version": "1.0.0"},
+    13: {"concept_id": None, "version": "1.0.0"},
 }
-
-GITHUB_REPO = "reincas/ameli"
-GITHUB_TOKEN = GitHubConfig()["ameli_token"]
-
-def github():
-    headers_gh = {
-        "Authorization": f"token {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-    gh_api_url = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
-    response = requests.get(gh_api_url, headers=headers_gh)
-    response.raise_for_status()
-    data = response.json()
-    release_name = data["tag_name"]
-    assets = [
-        {'name': f"{data['tag_name']}.zip", 'url': data['zipball_url']},
-        {'name': f"{data['tag_name']}.tar.gz", 'url': data['tarball_url']}
-    ]
-    return release_name, assets
-
 
 if __name__ == "__main__":
 
-    for num_electrons in [1]:  # get_configs():
+    for num_electrons in get_configs():
         version = RECORDS[num_electrons]["version"]
         concept_id = RECORDS[num_electrons]["concept_id"]
         zenodo_lanthanide(num_electrons, version, concept_id, SANDBOX)
